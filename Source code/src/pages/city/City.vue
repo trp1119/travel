16c38a0f28400e13160ef00a1717e9546a04ec64
trp1119 <tang@trp1119.com>
2019-04-30 20:35:32 +0800

    提交文件

---
+++ Source code/src/pages/city/City.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list
+      :hotCities="hotCities"
+      :cities="cities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      hotCities: [],
+      cities: {},
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ Source code/src/pages/detail/Detail.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :gallaryImgs="gallaryImgs"
+    ></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :categoryList="categoryList"></detail-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      categoryList: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        for (let i of data) {
+          if (i.id === this.$route.params.id) {
+            this.sightName = i.content.sightName
+            this.bannerImg = i.content.bannerImg
+            this.gallaryImgs = i.content.gallaryImgs
+            this.categoryList = i.content.categoryList
+          }
+        }
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .content
+    height: 50rem
+</style>
